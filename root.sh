0233f5673c4241dec0629e9eedd763f00fdcde9c
Geramie Kuba <gkuba@tuxgamer.com>
2022-10-09 14:16:49 -0600

    removed sudo from command

--- root.sh
+++ root.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Install packages
-sudo apt install -y git curl unzip neofetch nala vim most fontconfig zsh zsh-autosuggestions zsh-syntax-highlighting fonts-firacode
+apt install -y git curl unzip neofetch nala vim most fontconfig zsh zsh-autosuggestions zsh-syntax-highlighting fonts-firacode
 
 # Setup Passwordless sudo
 sed -i 's/^%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL) NOPASSWD ALL/' /etc/sudoers
