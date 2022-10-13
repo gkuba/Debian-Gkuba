ddd33cdd66345c687e00622a77e70e3347b70cd6
gkuba <gkuba@tuxgamer.com>
2022-10-13 01:35:26 -0600

    made the update silent

--- root.sh
+++ root.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Sync packages and install updates if needed.
-apt update && apt upgrade
+apt update && apt upgrade -y
 
 # Install packages
 apt install -y git curl unzip neofetch nala vim most fontconfig zsh zsh-autosuggestions zsh-syntax-highlighting fonts-firacode
